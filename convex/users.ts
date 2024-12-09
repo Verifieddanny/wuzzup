c91a3c23dfe54ce907e3073ac7ba8bfb01d42aee
Verifieddanny <dannyclassic56@gmail.com>
2024-12-10 00:20:04 +0100

    [Feat: Clerk to Convex Integration]

---
+++ convex/users.ts
@@ -0,0 +1,46 @@
+import { mutation, query } from "./_generated/server";
+import { v } from "convex/values";
+
+export const createUser = mutation({
+  args: { 
+        userId: v.string(),
+        email: v.string(),
+        createdAt: v.number(),
+        name: v.optional(v.string()),
+        profileImage: v.optional(v.string()),
+   },
+  handler: async (ctx, args) => {
+    try {
+      const newUser = await ctx.db.insert("users", {
+        userId: args.userId,
+        email: args.email,
+        createdAt: args.createdAt,
+        name: args.name,
+        profileImage: args.profileImage,
+    })
+
+    return newUser;
+    } catch (error) {
+      throw new Error("User information did not insert succesfully")
+    }
+   
+  },
+});
+
+
+export const readUser = query({
+  args: {
+    userId: v.string(),
+  },
+  handler: async (ctx, args) => {
+      try {
+        const userInfo = await ctx.db.query("users").filter((user) => {
+          return user.eq(user.field("userId"), args.userId)
+        }).first();
+
+        return userInfo;
+      } catch (error) {
+        throw new Error("reading user did not work")
+      }
+  }
+})
